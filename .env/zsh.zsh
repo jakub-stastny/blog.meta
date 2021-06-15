ff22b3caf669ec5c88d7dd5eb6be3fb9b2580eb7
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-15 12:44:03 +0000

    Fn regenerate-icons

--- .env/zsh.zsh
+++ .env/zsh.zsh
@@ -6,6 +6,12 @@ start-emacs-session
 rename-first-tab
 
 # Custom functions & aliases.
-# ...
+
+# Usage: regenerate-icons 32x32
+regenerate-icons() {
+  for file in doc/icons/*.orig.png; do
+    convert -resize $1 $file $(echo $file | sed -E 's/.orig//')
+  done
+}
 
 report-custom-functions
